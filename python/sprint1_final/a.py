c9c013cf1cef643f6442ea99d01787663982c1c1
Mirroxum-vent <director.kernelix@yandex.ru>
2022-08-02 13:56:23 +1000

    algor a

--- python/sprint1_final/a.py
+++ python/sprint1_final/a.py
@@ -1,31 +1,32 @@
-#ID 69546427
-def main(data):
-    result = [float('inf')] * len(data)
+def main(count_house, data):
+    result = [float('inf')] * count_house
     count = 0
-    for position in range(len(data)):
-        if int(data[position]) == 0:
+    position_zero = 0
+    for position in range(count_house):
+        if data[position] == 0:
             result[position] = 0
             count = 1
+            if position_zero is None:
+                position_zero = position
             if position != 0:
-                position -= 1
-                while (position != -1
-                       and result[position] > count):
-                    result[position] = count
-                    position -= 1
+                for pos in range(position-1, position - (position-position_zero)//2-1, -1):
+                    result[pos] = count
+                    print(result)
                     count += 1
                 count = 1
+            position_zero = position
             continue
         if count:
             result[position] = count
+            print(result)
             count += 1
     return result
 
 
 def read_data():
-    n = int(input())
-    return input().split()
+    return int(input()), [int(num) for num in input().split()]
 
 
 if __name__ == '__main__':
-    data = read_data()
-    [print(number, end=' ') for number in main(data)]
+    count_house, data = read_data()
+    [print(number, end=' ') for number in main(count_house, data)]
--- python/sprint1_final/b.py
+++ python/sprint1_final/b.py
@@ -1,23 +1,18 @@
-#ID 69546434
-import sys
-
-
-def main(n, data):
-    data = data.replace('.', '')
+#ID 69556020
+def main(count, data):
     result_dict = {}
     for position in range(len(data)):
-        if data[position] not in result_dict:
-            result_dict[data[position]] = 1
-        else:
-            result_dict[data[position]] += 1
-    return sum(j <= n*2 for j in result_dict.values())
+        result_dict.setdefault(data[position], 0)
+        result_dict[data[position]] += 1
+    return sum(j <= count*2 for j in result_dict.values())
 
 
 def read_data():
-    n = int(input())
-    return n, ''.join(sys.stdin.readline().rstrip() for _ in range(4))
+    count = int(input())
+    data = ''.join(input() for _ in range(4))
+    return count, data.replace('.', '')
 
 
 if __name__ == '__main__':
-    n, data = read_data()
-    print(main(n, data))
+    count, data = read_data()
+    print(main(count, data))
