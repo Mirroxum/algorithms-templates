44664d2e3d6784891123ba59c3eb1904e051031b
Mirroxum-vent <director.kernelix@yandex.ru>
2022-09-02 17:59:16 +1000

    13sprint fin review

--- python/mysocket.py
+++ python/mysocket.py
@@ -3,13 +3,14 @@ import socket
 
 def run():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(('localhost', 5000))
     server_socket.listen()
 
     while True:
         client_socket, addr = server_socket.accept()
         request = client_socket.recv(1024)
-        print(request)
+        print(request.decode('UTF-8'))
         print()
         print(addr)
 
